eccc0d126997232a242ac4ed6f45b03895dee642
Alex Kent <alex.kent1@hotmail.com>
2021-07-27 19:08:22 +0100

    Added REQUIRE macro and some re-formatting.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,13 @@
 #include "passage.h"
-#include <iostream>
 
 static int add(int a, int b)
 {
-	return a + b;
+  return a + b;
 }
 
 UNIT_TEST(addition_test)
 {
-	if (add(1, 1) == 3)
-	{
-		return passage::TestResult::SUCCESS;
-	}
-	else
-	{
-		return passage::TestResult::FAIL;
-	}
-}
+  REQUIRE(add(1, 1) == 2);
 
-int main()
-{
-	return static_cast<int>(passage::Session::GetInstance().Run());
+  return passage::TestResult::SUCCESS;
 }
--- passage.h
+++ passage.h
@@ -9,68 +9,78 @@ namespace passage
 
 enum class TestResult
 {
-	SUCCESS = 0,
-	FAIL
+  SUCCESS = 0,
+  FAIL
 };
 
 class Session
 {
 public:
-	using Test = std::function<TestResult()>;
+  using Test = std::function<TestResult()>;
 
-	static Session& GetInstance();
+  static Session& GetInstance();
 
-	void AddTest(const Test&);
+  void AddTest(const Test&);
 
-	TestResult Run() const;
+  TestResult Run() const;
 
 private:
-	std::vector<Test> tests_;
+  std::vector<Test> tests_;
 
   Session() = default;
-	Session(const Session&) = delete;
-	Session(Session&&) = delete;
+  Session(const Session&) = delete;
+  Session(Session&&) = delete;
 };
 
 Session& Session::GetInstance()
 {
-	static Session main_session_;
-	return main_session_;
+  static Session main_session_;
+  return main_session_;
 }
 
 void Session::AddTest(const Test& test)
 {
-	tests_.push_back(test);
+  tests_.push_back(test);
 }
 
 TestResult Session::Run() const
 {
-	auto res = TestResult::SUCCESS;;
-	for (const Test& test : tests_)
-	{
-		if (test() == TestResult::FAIL)
-		{
-			res = TestResult::FAIL;
-		}
-	}
+  auto res = TestResult::SUCCESS;;
+  for (const Test& test : tests_)
+  {
+    if (test() == TestResult::FAIL)
+    {
+      res = TestResult::FAIL;
+    }
+  }
 
-	return res;
+  return res;
 }
 
 class TestAdder
 {
 public:
-	TestAdder(const Session::Test& test)
-	{
-		Session::GetInstance().AddTest(test);
-	}
+  TestAdder(const Session::Test& test)
+  {
+    Session::GetInstance().AddTest(test);
+  }
 };
 
 }
 
+// The passage library defines main.
+int main()
+{
+  return static_cast<int>(passage::Session::GetInstance().Run());
+}
+
 #define UNIT_TEST(name) \
-	static passage::TestResult name(); \
-	static passage::TestAdder adder_##name(&name); \
-	static passage::TestResult name()
+  static passage::TestResult name(); \
+  static passage::TestAdder adder_##name(&name); \
+  static passage::TestResult name()
+
+#define REQUIRE(expression) \
+  if (!(expression)) \
+    return passage::TestResult::FAIL;
 
 #endif // __PASSAGE_H_INCLUDED__
